757984eb1fab5c00b1e366e1f3729087c0d48d84
Queency <delenaqueency@example.com>
2022-05-03 14:56:23 +0800

    transaction table hover

--- Expense-Tracker-aspnetcore/Startup.cs
+++ Expense-Tracker-aspnetcore/Startup.cs
@@ -72,7 +72,7 @@ namespace Expense_Tracker_aspnetcore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Transactions}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
